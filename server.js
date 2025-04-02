21bbf8a4645ec7eb70822c5e908d8e54e06ec001
saurabh-idea <n73307836@gmail.com>
2025-04-03 00:07:23 +0530

    server.js

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
+const express = require("express");
+const http = require("http");
 const WebSocket = require("ws");
 
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
 const PORT = process.env.PORT || 3000;
-const server = new WebSocket.Server({ port: PORT });
 
-let clients = new Set();
+wss.on("connection", (ws) => {
+    console.log("New client connected");
 
-server.on("connection", (socket) => {
-    console.log("New client connected!");
-    clients.add(socket);
-
-    socket.on("message", (message) => {
-        console.log("Received: " + message);
-        // Broadcast message to all clients except sender
-        clients.forEach(client => {
-            if (client !== socket && client.readyState === WebSocket.OPEN) {
+    ws.on("message", (message) => {
+        console.log(`Received: ${message}`);
+        wss.clients.forEach(client => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
                 client.send(message);
             }
         });
     });
 
-    socket.on("close", () => {
-        console.log("Client disconnected!");
-        clients.delete(socket);
+    ws.on("close", () => {
+        console.log("Client disconnected");
     });
 });
 
-console.log(`Signaling Server running on port ${PORT}`);
+server.listen(PORT, () => {
+    console.log(`Signaling Server running on port ${PORT}`);
+});
